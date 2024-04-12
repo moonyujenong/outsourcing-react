570fb9270334cc839780f9e42514a3973f2fc86e
cosdev69 <cosdev69@cosmo.cup>
2024-04-12 16:06:47 +0900

    초기화

--- piece/src/components/Layout/Body/index.jsx
+++
@@ -1,9 +0,0 @@
-import style from "./index.module.css";
-
-function Body ({children}) {
-  return (
-    <div className={style.container}>{children}</div>
-  )
-}
-
-export default Body;
--- piece/src/components/Layout/Footer/index.jsx
+++
@@ -1,9 +0,0 @@
-import style from "./index.module.css";
-
-function Footer () {
-  return (
-    <div className={style.container}>footer</div>
-  )
-}
-
-export default Footer;
