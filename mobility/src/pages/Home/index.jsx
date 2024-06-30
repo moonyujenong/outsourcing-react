2c7ef113ec5f6b88726497c63c2f8f1f079b15a2
moon <moon@moonui-iMac.local>
2024-06-30 20:17:33 +0900

    소개 섹션 완료

--- mobility/src/pages/Home/index.jsx
+++ mobility/src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 import style from "./index.module.css";
 import Layouts from "../../common/components/Layouts";
 import BannerSection from "./BannerSection";
+import IntroduceSection from "./IntroduceSection";
 
 function Home () {
   return (
     <Layouts>
       <div className={style.container}>
         <BannerSection />
+        <IntroduceSection />
       </div>
     </Layouts>
   )
