a9deefa6be8b47dedf8507870867b9600894ec88
hal <pn_frt_hal>
2024-10-18 10:43:09 +0900

    about tabline 퍼블

--- cj-one-group/src/pages/About/index.jsx
+++ cj-one-group/src/pages/About/index.jsx
@@ -7,6 +7,13 @@ import visualImgTest from "../../assets/images/about/about-visual-banner.png";
 import missionImg1 from "../../assets/images/about/mission-img-1.png";
 import missionImg2 from "../../assets/images/about/mission-img-2.png";
 import missionImg2Mobile from "../../assets/images/about/mission-img-2-mobile.png";
+import guidelineMobile from "../../assets/images/about/logo-guideline-mobile.png";
+import guidelinePc from "../../assets/images/about/logo-guideline-pc.png";
+import lineBannerMobile from "../../assets/images/about/line-banner-mobile.png";
+import lineBannerPc from "../../assets/images/about/line-banner-pc.png";
+import taglineImg1 from "../../assets/images/about/tagline-img-1.png";
+import taglineImg2 from "../../assets/images/about/tagline-img-2.png";
+import taglineImg3 from "../../assets/images/about/tagline-img-3.png";
 
 function About () {
     const innerWidth = useResponsive();
@@ -51,7 +58,30 @@ function About () {
                         <img alt="mission img 1" src={missionImg1} />
                         <img alt="mission img 2" src={innerWidth >= 1024 ? missionImg2 : missionImg2Mobile} />
                     </div>
+                </div>
+                <div className={style.guide_line}>
+                    <img alt="logo guide line img" src={innerWidth >= 1024 ? guidelinePc:guidelineMobile} />
+                </div>
 
+                <img style={{width:'100%', display:'block'}} alt="line banner" src={innerWidth >= 1024 ? lineBannerPc:lineBannerMobile} />
+
+                <div className={style.tagline_section}>
+                    <div className={style.img_wrap}>
+                        <span>
+                            <p>BRAND TAGLINE</p>
+                            <img alt="tagline img 1" src={taglineImg1} />
+                        </span>
+                        <span>
+                            <p>Stories In Every Flavor,</p>
+                            <img alt="tagline img 2" src={taglineImg2} />
+                        </span>
+                        <span>
+                            <p>Memories In Every Bite</p>
+                            <img alt="tagline img 3" src={taglineImg3} />
+                        </span>
+                    </div>
+                    <h3 className={style.title}>BRAND TAGLINE</h3>
+                    <p>Stories in Every Flavor,<br />Memories in Every Bite</p>
                 </div>
             </div>
         </Layouts>
